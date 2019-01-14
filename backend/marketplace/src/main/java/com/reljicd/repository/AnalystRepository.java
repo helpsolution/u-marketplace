960f40ba98ee22f1b404769b3e0e46a01308c348
orlov <alex567.alex@yandex.ru>
2019-01-14 22:26:23 +0300

    file uploading done

--- backend/marketplace/src/main/java/com/reljicd/repository/AnalystRepository.java
+++ backend/marketplace/src/main/java/com/reljicd/repository/AnalystRepository.java
@@ -1,12 +1,13 @@
 package com.reljicd.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.reljicd.model.Analyst;
-import com.reljicd.model.User;
 
 public interface AnalystRepository extends JpaRepository<Analyst, Long> {
+
+    @Query(nativeQuery = true, value = "select a.* from Analyst a join user_of_system u on u.id=a.user_of_system_id where u.username = :username")
+    Analyst findByUsername(@Param("username") String username);
 }
